efa160cfbb3a1cd42819591ac360120b5e3d6763
niha <niha8515@yahoo.com>
2022-02-05 05:33:11 -0800

    q1_interfaces introduce

---
+++ src/main/kotlin/q1Vehicle.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+}
+interface PublicTransportationVehicle {
+    fun maxSpeed()
+    fun maxCapacity()
+}
+interface ServiceTransportationVehicle{
+    fun maxSpeed()
+    fun maxCapacity()
+}
+interface PrivateTransportationVehicle{
+    fun maxSpeed()
+    fun maxCapacity()
+}
