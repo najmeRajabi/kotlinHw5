208c6e54d58241b9325c24ce9c3077cae33a6271
niha <niha8515@yahoo.com>
2022-02-05 23:45:49 -0800

    q1_classes introduce

--- src/main/kotlin/q1Vehicle.kt
+++ src/main/kotlin/q1Vehicle.kt
@@ -13,3 +13,57 @@ interface PrivateTransportationVehicle{
     fun maxSpeed()
     fun maxCapacity()
 }
+class Taxi:PublicTransportationVehicle{
+    override fun maxSpeed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun maxCapacity() {
+        TODO("Not yet implemented")
+    }
+}
+class Bus:PublicTransportationVehicle{
+    override fun maxSpeed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun maxCapacity() {
+        TODO("Not yet implemented")
+    }
+}
+class Ambulance:ServiceTransportationVehicle{
+    override fun maxSpeed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun maxCapacity() {
+        TODO("Not yet implemented")
+    }
+}
+class Truck:ServiceTransportationVehicle{
+    override fun maxSpeed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun maxCapacity() {
+        TODO("Not yet implemented")
+    }
+}
+class SportCar:PrivateTransportationVehicle{
+    override fun maxSpeed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun maxCapacity() {
+        TODO("Not yet implemented")
+    }
+}
+class Van:PrivateTransportationVehicle{
+    override fun maxSpeed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun maxCapacity() {
+        TODO("Not yet implemented")
+    }
+}
