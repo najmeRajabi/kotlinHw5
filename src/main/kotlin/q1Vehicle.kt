fb34400e9103cf309bccc2ae551fc9c392785d90
niha <niha8515@yahoo.com>
2022-02-06 05:59:19 -0800

    q1_create vehicles abstract

--- src/main/kotlin/q1Vehicle.kt
+++ src/main/kotlin/q1Vehicle.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val vehicleList = arrayListOf<Any>()
+    val vehicleList = arrayListOf<Vehicles>()
     val yellowTaxi = Taxi()
     val amirTaxi = Taxi()
     val beheshtiSchoolBus =Bus()
@@ -10,80 +10,98 @@ fun main() {
     val aliCar = SportCar()
     val greenVan = Van()
     val johnVan = Van()
+    vehicleList.addAll(arrayOf(yellowTaxi,amirTaxi,beheshtiSchoolBus,
+        redBus,gharaziAmbulance,saeedTruck,goliCar,aliCar,greenVan,johnVan))
+    var sortedBaseCapacity = arrayListOf<Vehicles>()
+    for (item in 0 until vehicleList.size){
+        if (vehicleList[item] > vehicleList[item+1]){}
+    }
 
+
+}
+abstract class Vehicles:Comparable<Vehicles>{
+    override fun compareTo(other: Vehicles): Int {
+        var result = 0
+        if (this is Truck ){
+            result = -1
+        }else if (this is SportCar){
+            result = 1
+        }
+        return result
+    }
 }
 interface Vehicle{
     val name :String
 }
 interface PublicTransportationVehicle:Vehicle {
-    fun maxSpeed()
-    fun maxCapacity()
+    fun maxSpeed():Int
+    fun maxCapacity():Int
     override val name: String
         get() = TODO("Not yet implemented")
 }
 interface ServiceTransportationVehicle:Vehicle{
-    fun maxSpeed()
-    fun maxCapacity()
+    fun maxSpeed():Int
+    fun maxCapacity():Int
     override val name: String
         get() = TODO("Not yet implemented")
 }
 interface PrivateTransportationVehicle:Vehicle{
-    fun maxSpeed()
-    fun maxCapacity()
+    fun maxSpeed():Int
+    fun maxCapacity():Int
     override val name: String
         get() = TODO("Not yet implemented")
 }
-class Taxi:PublicTransportationVehicle{
-    override fun maxSpeed() {
-        TODO("Not yet implemented")
+class Taxi:PublicTransportationVehicle, Vehicles() {
+    override fun maxSpeed():Int {
+        return 100
     }
 
-    override fun maxCapacity() {
-        TODO("Not yet implemented")
+    override fun maxCapacity():Int {
+        return 4
     }
 }
-class Bus:PublicTransportationVehicle{
-    override fun maxSpeed() {
-        TODO("Not yet implemented")
+class Bus:PublicTransportationVehicle,Vehicles(){
+    override fun maxSpeed():Int {
+        return 80
     }
 
-    override fun maxCapacity() {
-        TODO("Not yet implemented")
+    override fun maxCapacity():Int {
+        return 42
     }
 }
-class Ambulance:ServiceTransportationVehicle{
-    override fun maxSpeed() {
-        TODO("Not yet implemented")
+class Ambulance:ServiceTransportationVehicle,Vehicles(){
+    override fun maxSpeed():Int {
+        return 120
     }
 
-    override fun maxCapacity() {
-        TODO("Not yet implemented")
+    override fun maxCapacity():Int {
+        return 5
     }
 }
-class Truck:ServiceTransportationVehicle{
-    override fun maxSpeed() {
-        TODO("Not yet implemented")
+class Truck:ServiceTransportationVehicle,Vehicles(){
+    override fun maxSpeed():Int {
+        return 70
     }
 
-    override fun maxCapacity() {
-        TODO("Not yet implemented")
+    override fun maxCapacity():Int {
+        return 1
     }
 }
-class SportCar:PrivateTransportationVehicle{
-    override fun maxSpeed() {
-        TODO("Not yet implemented")
+class SportCar:PrivateTransportationVehicle,Vehicles(){
+    override fun maxSpeed():Int {
+        return 300
     }
 
-    override fun maxCapacity() {
-        TODO("Not yet implemented")
+    override fun maxCapacity():Int {
+        return 2
     }
 }
-class Van:PrivateTransportationVehicle{
-    override fun maxSpeed() {
-        TODO("Not yet implemented")
+class Van:PrivateTransportationVehicle,Vehicles(){
+    override fun maxSpeed():Int {
+        return 90
     }
 
-    override fun maxCapacity() {
-        TODO("Not yet implemented")
+    override fun maxCapacity():Int {
+        return 15
     }
 }
