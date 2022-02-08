5b1978ab58bc50ab70ea545cac13407a58471a31
niha <niha8515@yahoo.com>
2022-02-08 00:05:55 -0800

    q2_introduce mainClass

--- src/main/kotlin/q2.kt
+++ src/main/kotlin/q2.kt
@@ -14,28 +14,28 @@ abstract class Employee{
         return "name: $name age: $age hourRate: $hourRate"
     }
 }
-class Manager:Employee(){
-    override val name: String
-        get() = name
-    override val age: Int
-        get() = age
-    override val hourRate: Double
-        get() = hourRate
+class Manager(override val name: String, override val age: Int,
+              override val hourRate: Double):Employee(){
 
     override fun salary(hours: Double): Double {
         return hourRate*hours
     }
 }
-class Clerk():Employee(){
-    override val name: String
-        get() = name
-    override val age: Int
-        get() = age
-    override val hourRate: Double
-        get() = hourRate
+class Clerk(override val name: String,override val age: Int,
+            override val hourRate: Double):Employee(){
 
     override fun salary(hours: Double): Double {
         return  hourRate*hours/2
     }
 }
 data class Customer(var name :String,var age:Int,var gender:Gender){}
+class Main1{
+    var ali = Manager("Ali",25,104.0)
+    var reza = Manager("Reza",42,120.0)
+    var farhad = Manager("Farhad",34,120.0)
+    val sama = Clerk("Sama" , 26 , 110.0)
+    val hours= arrayListOf(140,150,130,180)
+    val sara = Customer("sara",22,Gender.Female)
+    val maryam = Customer("maryam" , 30 , Gender.Female)
+
+}
