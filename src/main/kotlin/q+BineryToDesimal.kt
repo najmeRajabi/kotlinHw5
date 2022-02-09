75f1490da7a262be371df136068064c1c51104a7
niha <niha8515@yahoo.com>
2022-02-09 04:29:33 -0800

    q+_create fun decimal->binary2

--- src/main/kotlin/q+BineryToDesimal.kt
+++ src/main/kotlin/q+BineryToDesimal.kt
@@ -1,6 +1,7 @@
 fun main() {
     println(binaryToDecimal("11001"))
     println(decimalToBinary(439))
+    println(convertDecimalToBinary(439))
 
 }
 fun binaryToDecimal(binary:String):Int{
@@ -8,4 +9,20 @@ fun binaryToDecimal(binary:String):Int{
 }
 fun decimalToBinary(number: Int): String {
     return Integer.toBinaryString(number)
+}
+
+
+fun convertDecimalToBinary(number: Int):Long{
+    var number =number
+    var binary:Long=0
+    var mode :Int
+    var i = 1
+
+    while (number !=0){
+        mode = number % 2
+        number /= 2
+        binary += (mode *i).toLong()
+        i *= 10
+    }
+    return binary
 }
