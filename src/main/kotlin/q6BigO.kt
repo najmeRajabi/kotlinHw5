2f6ab7352d6a0dff7881f12a3976381a2580017c
niha <niha8515@yahoo.com>
2022-02-09 03:45:09 -0800

    q6_write and define big O

--- src/main/kotlin/q6BigO.kt
+++ src/main/kotlin/q6BigO.kt
@@ -1,3 +1,31 @@
 fun main() {
 
+    // BigO = O 1
+    val n =5
+    println("Your input is:$n")
+
+    //....O n
+    for (i in 0 until n){
+        println("Your input is:$i")
+    }
+
+    //...O n^2....
+    for (i in 1..n){
+        for (j in 1..n){
+            println("Your input is:$i and $j")
+        }
+    }
+
+    //..O n^2.............
+    for (i in 1..factorial(n)){
+        println("Your input is:$i")
+    }
+
+
+}
+fun factorial(n:Int):Int{
+    var result=1
+    for (i in n downTo 1)
+        result *= i
+    return result
 }
