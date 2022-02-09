35fe890588fa9d3fde00cee81c58532efecade4e
niha <niha8515@yahoo.com>
2022-02-09 01:03:24 -0800

    q5_warning elimination

--- src/main/kotlin/q5.kt
+++ src/main/kotlin/q5.kt
@@ -8,8 +8,8 @@ fun main() {
 
 }
 fun <T> ArrayList<T>.slice( number: Int){
-    var list1 = arrayListOf<T>()
-    var list2 = arrayListOf<T>()
+    val list1 = arrayListOf<T>()
+    val list2 = arrayListOf<T>()
     if (this.size-1 > number){
         for (item in 0..number){
             list1 .add(this[item])
