8c673732e3a7f8d0509e8b6d7f40d97a81612ccc
niha <niha8515@yahoo.com>
2022-02-09 04:06:50 -0800

    q+_create fun decimal->binary

--- src/main/kotlin/q+BineryToDesimal.kt
+++ src/main/kotlin/q+BineryToDesimal.kt
@@ -1,7 +1,11 @@
 fun main() {
     println(binaryToDecimal("11001"))
+    println(decimalToBinary(439))
 
 }
 fun binaryToDecimal(binary:String):Int{
     return binary.toInt(2)
+}
+fun decimalToBinary(number: Int): String {
+    return Integer.toBinaryString(number)
 }
